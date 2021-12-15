04d4369aa4fc39148d5a3c4f2ae7c3408b596595
Ilolo Izu <iloloizu97@gmail.com>
2021-12-14 22:13:45 -0600

    commit tree c35fe65041bb3db0e4bed0a606455f7809d8e9e3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+// function findMatching(driver) {
+//     return driver.filter(driver => {
+//         return drivers === driver
+//     });
+// }
+
+function findMatching(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
